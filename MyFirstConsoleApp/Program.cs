79fd850072aae7a00a95e0244d3d55e28137b81c
johanngrabner-lv <johann.grabner@lv.campus02.at>
2022-12-15 10:18:11 +0100

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //lokale Variablen deklarien
+            string firstName; //string ist Alias für System.String
+            int plz; //int ist ein alias für System.Int32
+
+            System.String lastName="Grabner";
+            System.Int32 streetNumber;
+
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Wie heißen Sie?");
+
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Wo wohnen Sie? (PLZ)");
+
+            plz = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hallo und herzlich Willkommen " + firstName);
+            Console.WriteLine("Hallo {0} {1} herzlich Willkommen ", firstName, lastName);
+            Console.WriteLine($"Hallo {firstName} {lastName} herzlich Willkommen {plz}"); //
+
+
+
+        }
+    }
+}

608a9c80bfd8ba05c58c053ac9fcaf9ac419775a
johanngrabner-lv <johann.grabner@lv.campus02.at>
2022-12-15 11:11:37 +0100

    Zahlenratespiel

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -6,11 +6,96 @@ namespace MyFirstConsoleApp
     {
         static void Main(string[] args)
         {
+            Method1Einfuehrung();
+            Method2ArraysUndSchleifen();
+
+        }
+
+        private static void Method2ArraysUndSchleifen()
+        {
+            int[] zahlen = new int[4];
+            zahlen[0] = 1;
+            zahlen[2] = 7;
+
+            string[] produkte = { "Handy", "Taschenlampe", "Maus" };
+
+            if (produkte[0] == "Handy" || produkte[0].Equals("Handy"))
+            {
+                Console.WriteLine("Ja ein Handy");
+            }
+
+            switch (produkte[0])
+            {
+                case "Handy":
+                    Console.WriteLine("war ein Handy");
+                    break;
+                case "Maus":
+                    Console.WriteLine("war Maus");
+                    break;
+                default:
+                    break;
+            }
+
+            for (int i = 0; i < zahlen.Length; i++)
+            {
+                Console.WriteLine($"Die Zahl {i+1} hat den Wert {zahlen[i]}");
+            }
+
+            foreach (var zahl in zahlen)
+            {
+                Console.WriteLine(zahl);
+            }
+
+            int z1 = 12;
+            var z2 = 14;
+
+            int z3;
+           // var z4;
+
+            //Zahlenrate spiel- neue Methode "ZahlenRaten"
+            //Computer überlegt sich eine zufällige Zahl (0 bis 100) Random
+            //max. 10 Mal die Möglichkeit einen Tipp abzugeben
+            //Beispiel zufällige Zahl ist 37
+            //1. Tipp --> 50
+            //Ihre Eingabe ist höher als die zufällige Zahl
+            //Oje - Sie haben die Zahl nicht erraten
+            //Super - Sie haben die Zahl nach 3 Versuchen erraten
+
+
+
+
+
+
+        }
+
+        private static void Method1Einfuehrung()
+        {
             //lokale Variablen deklarien
             string firstName; //string ist Alias für System.String
-            int plz; //int ist ein alias für System.Int32
+            int plz = 0; //int ist ein alias für System.Int32
 
-            System.String lastName="Grabner";
+            double plzDbl = 0;
+
+            plzDbl = plz; //implizite Konvertierung
+
+            plzDbl = double.MaxValue;
+
+            checked
+            {
+                try
+                {
+                    plz = (int)plzDbl;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ein Fehler{ex.Message}");
+                    throw;
+                }
+
+            }
+
+
+            System.String lastName = "Grabner";
             System.Int32 streetNumber;
 
 
@@ -27,9 +112,6 @@ namespace MyFirstConsoleApp
             Console.WriteLine("Hallo und herzlich Willkommen " + firstName);
             Console.WriteLine("Hallo {0} {1} herzlich Willkommen ", firstName, lastName);
             Console.WriteLine($"Hallo {firstName} {lastName} herzlich Willkommen {plz}"); //
-
-
-
         }
     }
 }
